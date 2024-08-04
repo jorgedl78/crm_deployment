fdf0b104b74633a5d04b2b136161bd0c396cce1f
Jorge de León <jorge_dl@hotmail.com>
2024-08-04 01:26:30 -0300

    ajustes previos al deployment

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const whiteList = [process.env.FRONTEND_URL];
 //const whiteList = ['http://localhost:3000','http://otroServidor:xxxx'];
 const corsOptions = {
   origin: (origin, callback) => {
-    //console.log(origin);
+    console.log(origin);
     //Revisar si la petición viene de un servidor que está en whiteList
     const existe = whiteList.some(dominio => dominio === origin);
     if(existe) {
